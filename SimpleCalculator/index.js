547548843863e23fe3eb8eda781e62ae9a7da59c
Aditya Thorat <thorat.aditya@outlook.com>
2022-02-01 19:17:09 +0530

    Complete SimpleCalculator

--- SimpleCalculator/index.js
+++ SimpleCalculator/index.js
@@ -8,7 +8,17 @@ app.get("/", function(req, res) {
     res.sendFile(__dirname + "/index.html");
 });
 
-app.post("/", function() {
+app.post("/", function(req, res) {
+    console.log(req.body);
 
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+    var result = num1 + num2;
+
+    res.send("Addition of " + num1 + " and " + num2 + " is " + result);
 });
 
+
+app.listen(3000, function() {
+    console.log("Server is running on port 3000.");
+});
