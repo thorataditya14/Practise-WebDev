39bb1a728a04a7026d7666e741c79bd7aff4857d
Aditya Thorat <thorat.aditya@outlook.com>
2022-03-20 08:33:26 +0530

    Complete MailchimpAPI

---
+++ MailchimpAPI/index.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static("public"))
+
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/signup.html");
+
+})
+
+app.post("/", function(req, res) {
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const email = req.body.email;
+
+    const userData = {
+        members: [ {
+            email_address: email,
+            status: "subscribed",
+            merge_feilds: {
+                FNAME: firstName,
+                LNAME: lastName
+            }
+        } ]
+    };
+
+    const jsonData = JSON.stringify(userData);
+
+    const URL = "https://us14.api.mailchimp.com/3.0/lists/fab89d3120";
+
+    const options = {
+        method: "POST",
+        auth: "temp:6a40da3a2775329d3fd824c648d28508-us14"
+    }
+
+    const request = https.request(URL, options, function(response) {
+        console.log(response.statusCode);
+
+        if(response.statusCode === 200)
+            res.sendFile(__dirname + "/success.html");
+        else
+            res.sendFile(__dirname + "/failure.html");
+
+        response.on("data", function(data) {
+            console.log(JSON.parse(data));
+        });
+    });
+
+    request.write(jsonData);
+    request.end();
+});
+
+
+app.post("/failure", function(req,res) {
+    res.redirect("/");
+});
+
+
+app.listen(process.env.PORT ||  3000, function() {
+    console.log("Server is running");
+});
