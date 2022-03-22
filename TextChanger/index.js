8a33a2f2aaa08fc69f1c4e26f8b0814f81111e7c
Aditya Thorat <thorat.aditya@outlook.com>
2022-03-22 22:22:26 +0530

    Create TextChanger

---
+++ TextChanger/index.js
@@ -0,0 +1,8 @@
+function changeTheText() {
+
+    var changeTextColor = document.getElementById("changeUserColor").value;
+    document.getElementById("paragraphText").style.color = changeTextColor;
+
+    var changeTextVariable = document.getElementById("changeUserText").value;
+    document.getElementById("paragraphText").innerHTML = changeTextVariable;
+}
